3a021e359da20aa1859cbe40cdf524212b5a99e8
ar <a.rau@comlog.org>
2019-04-09 10:55:35 +0200

    ~ Custom Logger

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function ComlogFileTimeWatcher(options) {
 	this.min = -1; // Minimale anzahl der Dateien -1 == ignore
 	this.folder = false; // Ordner mit zählen
 
+	this.logger = console;
+
 	// Private funktionen
 	var _running = false, _timer = null;
 
@@ -42,31 +44,31 @@ function ComlogFileTimeWatcher(options) {
 		_running = true;
 
 		var p = path.normalize((typeof _self.path == 'function') ? _self.path() : _self.path);
-		if (_self.debug) console.info('Check '+p+' ...');
+		if (_self.debug) _self.logger.info('Check '+p+' ...');
 
 		fs.readdir(p, function (err, files) {
-			//console.info(files);
+			//_self.logger.info(files);
 			_count(path.normalize(p), files, 0, function (err, cnt) {
-				//console.info(arguments);
+				//_self.logger.info(arguments);
 				if (err !== null) {
-					if (_self.debug) console.error(err.stack || err);
+					if (_self.debug) _self.logger.error(err.stack || err);
 					_self.trigger('error', [new Error("FileCount error for "+p+" \n"+err.message)]);
 					if (_self.satus === true) _self.trigger('down');
 					_self.satus = false;
 				} else {
 					// Service is down
 					if (_self.max > -1 && cnt >= _self.max) {
-						if (_self.debug) console.info("FileCount overflow "+_self.max+" in "+p);
+						if (_self.debug) _self.logger.info("FileCount overflow "+_self.max+" in "+p);
 						if (_self.satus === true) _self.trigger('down');
 						_self.satus = false;
 					}
 					else if (_self.min > -1 && cnt <= _self.min) {
-						if (_self.debug) console.info("FileCount underflow "+_self.min+" in "+p);
+						if (_self.debug) _self.logger.info("FileCount underflow "+_self.min+" in "+p);
 						if (_self.satus === true) _self.trigger('down');
 						_self.satus = false;
 					}
 					else {
-						if (_self.debug) console.info("FileCount ok "+p);
+						if (_self.debug) _self.logger.info("FileCount ok "+p);
 						if (_self.satus === false) _self.trigger('up');
 						_self.satus = true;
 					}
